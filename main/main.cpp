0dda8aa6382b91f2e27b643a01db9e0fd335f964
YanNaingWinn <yann@oinsm.ch>
2026-04-23 20:44:18 +0700

    Fix MISRA violations in main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -103,7 +103,7 @@ static void show_uid_on_epd(uint32_t uid) {
 }
 
 extern "C" void app_main(void) {
-    uint8_t rnd[RND_BUF_SIZE];
+    uint8_t rnd[RND_BUF_SIZE] = { 0U };
     bool    rnd_ok = CryptnoxUtils::fill_secure_random(rnd, sizeof(rnd));
     if (rnd_ok) {
         ESP_LOGI(TAG, "TRNG: %02X%02X%02X%02X%02X%02X%02X%02X"
