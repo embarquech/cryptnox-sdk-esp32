56b5498a5c31eacedd69ae93e9bee8eb1dccdcad
YanNaingWinn <yann@oinsm.ch>
2026-04-29 00:40:36 +0700

    Fix cryptnox_utils component: restore header, remove esp_wifi dependency

--- components/cryptnox_utils/esp32_random.cpp
+++ components/cryptnox_utils/esp32_random.cpp
@@ -1,21 +1,11 @@
 #include "CryptnoxUtils.h"
 #include "esp_random.h"
-#include "esp_wifi.h"
 
-static bool wifi_is_active(void) {
-    wifi_mode_t mode = WIFI_MODE_NULL;
-    esp_err_t err = esp_wifi_get_mode(&mode);
-    bool wifi_active = ((err == ESP_OK) && (mode != WIFI_MODE_NULL));
-    return wifi_active;
-}
-
-bool CryptnoxUtils::fill_secure_random(uint8_t* dest, size_t len) {
-    bool wifi_ready = wifi_is_active();
-    bool is_ready = (dest != nullptr) && (len != 0U) && wifi_ready;
-
+/* Caller must ensure WiFi or Bluetooth is active to properly seed the hardware TRNG. */
+bool CryptnoxUtils::fill_secure_random(uint8_t *dest, size_t len) {
+    bool is_ready = ((dest != nullptr) && (len != 0U));
     if (is_ready) {
         esp_fill_random(dest, len);
     }
-
     return is_ready;
 }
